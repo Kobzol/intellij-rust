6f9302b29dd8d553aef67b31d13eb5343764991e
Jakub Beránek <berykubik@gmail.com>
2021-07-18 17:49:15 +0200

    INT: add intention to add an alias to an import

--- src/main/kotlin/org/rust/ide/inspections/lints/RsPathUsageAnalysis.kt
+++ src/main/kotlin/org/rust/ide/inspections/lints/RsPathUsageAnalysis.kt
@@ -31,24 +31,34 @@ val RsItemsOwner.pathUsage: PathUsageMap
         CachedValueProvider.Result.create(usages, PsiModificationTracker.MODIFICATION_COUNT)
     }
 
+sealed class PathUsageBase {
+    data class PathUsage(val name: String, val target: RsElement, val source: PsiElement?) : PathUsageBase()
+    data class TraitUsage(val trait: RsTraitItem) : PathUsageBase()
+}
+
+fun traversePathUsages(owner: RsItemsOwner, processor: (PathUsageBase) -> Unit) {
+    for (child in owner.children) {
+        handleSubtree(child, processor)
+    }
+}
+
 private fun calculatePathUsages(owner: RsItemsOwner): PathUsageMap {
     val directUsages = hashMapOf<String, MutableSet<RsElement>>()
     val traitUsages = hashSetOf<RsTraitItem>()
 
-    for (child in owner.children) {
-        handleSubtree(child, directUsages, traitUsages)
+    traversePathUsages(owner) { usage ->
+        when (usage) {
+            is PathUsageBase.PathUsage -> directUsages.getOrPut(usage.name) { hashSetOf() }.add(usage.target)
+            is PathUsageBase.TraitUsage -> traitUsages.add(usage.trait)
+        }
     }
 
     return PathUsageMap(directUsages, traitUsages)
 }
 
-private fun handleSubtree(
-    root: PsiElement,
-    directUsages: MutableMap<String, MutableSet<RsElement>>,
-    traitUsages: MutableSet<RsTraitItem>
-) {
+private fun handleSubtree(root: PsiElement, processor: (PathUsageBase) -> Unit) {
     processElementsWithMacros(root) { element ->
-        if (handleElement(element, directUsages, traitUsages)) {
+        if (handleElement(element, processor)) {
             TreeStatus.VISIT_CHILDREN
         } else {
             TreeStatus.SKIP_CHILDREN
@@ -56,15 +66,7 @@ private fun handleSubtree(
     }
 }
 
-private fun handleElement(
-    element: PsiElement,
-    directUsages: MutableMap<String, MutableSet<RsElement>>,
-    traitUsages: MutableSet<RsTraitItem>
-): Boolean {
-    fun addItem(name: String, item: RsElement) {
-        directUsages.getOrPut(name) { hashSetOf() }.add(item)
-    }
-
+private fun handleElement(element: PsiElement, processor: (PathUsageBase) -> Unit): Boolean {
     if (!element.isEnabledByCfg) return false
 
     return when (element) {
@@ -73,14 +75,16 @@ private fun handleElement(
             val name = element.patBinding.referenceName
             val targets = element.patBinding.reference.multiResolve()
             targets.forEach {
-                addItem(name, it)
+                processor(PathUsageBase.PathUsage(name, it, element.patBinding.referenceNameElement))
             }
             true
         }
         is RsPath -> {
             if (element.qualifier != null) {
                 val requiredTraits = getAssociatedItemRequiredTraits(element).orEmpty()
-                traitUsages.addAll(requiredTraits)
+                requiredTraits.forEach {
+                    processor(PathUsageBase.TraitUsage(it))
+                }
             } else {
                 val useSpeck = element.parentOfType<RsUseSpeck>()
                 if (useSpeck == null || useSpeck.isTopLevel) {
@@ -88,19 +92,21 @@ private fun handleElement(
                     if (name in IGNORED_USE_PATHS) return true
                     val targets = element.reference?.multiResolve().orEmpty()
                     targets.forEach {
-                        addItem(name, it)
+                        processor(PathUsageBase.PathUsage(name, it, element.referenceNameElement))
                     }
                 }
             }
             true
         }
         is RsMacroCall -> {
-            handleSubtree(element.path, directUsages, traitUsages)
+            handleSubtree(element.path, processor)
             true
         }
         is RsMethodCall -> {
             val requiredTraits = getMethodRequiredTraits(element).orEmpty()
-            traitUsages.addAll(requiredTraits)
+            requiredTraits.forEach {
+                processor(PathUsageBase.TraitUsage(it))
+            }
             true
         }
         else -> true
